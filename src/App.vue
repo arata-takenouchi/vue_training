f8a0f110634e18061e864f60664a921c92177122
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-03-28 23:30:26 +0900

    nextTick

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
-  <div>
-    <h1>Hello World</h1>
+  <div id="counter">
+    {{ count }}
   </div>
+  <button @click="increment">increment</button>
 </template>
 
 <script setup lang="ts">
+import { ref, nextTick } from 'vue'
 
+const count = ref(0)
+
+async function increment() {
+  count.value++
+  console.log(document.getElementById('counter')!.textContent)
+  await nextTick()
+  console.log(document.getElementById('counter')!.textContent)
+}
 </script>
