294a6294f013928169328aeb7b99fd95e19d8b0f
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-04-02 00:02:37 +0900

    v-model options

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <template>
-	<BlogPost
+	<!-- <BlogPost
   	v-for="post in posts"
 	  :key="post.id"
   	:title="post.title"
-	></BlogPost>
+	></BlogPost> -->
+  {{ firstName }} {{ lastName }} {{ capitalizeModel }}
+  <Test
+    v-model="countModel"
+    v-model:first-name="firstName"
+    v-model:last-name="lastName"
+    v-model:capitalize-model.capitalize="capitalizeModel" />
+  <div>
+    <button @click="update">Increment In Parent</button>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import BlogPost from './BlogPost.vue'
+import Test from './Test.vue'
+
+const countModel = ref(1)
+const firstName = ref('arata')
+const lastName = ref('takenouchi')
+// const [modelValue, modelModifires] = defineModel()
+const capitalizeModel = ref('capitalizeModel')
 
 const posts = ref([
   { id: 1, title: 'My journey with Vue' },
   { id: 2, title: 'Blogging with Vue' },
   { id: 3, title: 'Why Vue is so fun' }
 ])
+
+function update() {
+  countModel.value += 1
+}
 </script>
---
+++ src/Test.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>v-model is {{ model }}</div>
+  <div>
+    <button @click="update">Increment In Children</button>
+  </div>
+  <div>
+    <input type="text" v-model="firstName" />
+    <input type="text" v-model="lastName" />
+    <input type="text" v-model="capitalizeModel" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const model = defineModel({required: true})
+const firstName = defineModel('firstName', {required: true})
+const lastName = defineModel('lastName', {required: true})
+const [capitalizeModel, modifiers] = defineModel('capitalizeModel', {
+  set(value) {
+    if (modifiers.capitalize) {
+      return value.toUpperCase()
+    }
+    return value
+  }
+})
+
+function update() {
+  model.value += 1
+}
+</script>
