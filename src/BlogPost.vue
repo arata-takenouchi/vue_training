beca359eb368961d528b1bad7e0fd7a1023e1584
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-04-01 21:47:44 +0900

    $emit

--- src/BlogPost.vue
+++ src/BlogPost.vue
@@ -1,7 +1,9 @@
 <template>
   <h4>{{ title }}</h4>
+  <button @click="$emit('large-text')">large-text</button>
 </template>
 
 <script lang="ts" setup>
 defineProps(['title'])
+defineEmits(['large-text'])
 </script>
