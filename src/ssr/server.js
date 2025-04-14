8f1ca2bc2843184f5338568c8783313befa0c3a8
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-04-14 22:08:45 +0900

    ssr test2

---
+++ src/ssr/server.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import { createApp } from './app.js'
+import { renderToString } from 'vue/server-renderer'
+
+const server = express()
+server.use(express.static('.'))
+
+server.get('/', (req, res) => {
+  const app = createApp()
+
+  renderToString(app).then((html) => {
+    res.send(`
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>Vue SSR Example</title>
+          <script type="module" src="/client.js"></script>
+        </head>
+        <body>
+          <div id="app">${html}</div>
+        </body>
+      </html>
+    `)
+  })
+})
+
+server.listen(3000, () => {
+  console.log('ready')
+})
