8fe4cf6384a62baf6de3bbf822c5fd75e26f62ce
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-04-17 00:00:33 +0900

    async component 2

--- src/async-component.ts
+++ src/async-component.ts
@@ -1,5 +1,13 @@
 import { defineAsyncComponent } from 'vue'
 
-const AsyncComp = defineAsyncComponent(() =>
-  import('./Test.vue')
-)
+const AsyncComp = defineAsyncComponent({
+  loader: () => import('./Test.vue'),
+
+  loadingComponent: LoadingComponent,
+
+  delay: 200,
+
+  errorComponent: ErrorComponent,
+
+  timeout: 3000
+})
