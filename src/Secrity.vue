314351d546f87c4b0ab4e6bc38b4ca5475b73af1
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-04-19 16:44:55 +0900

    security

---
+++ src/Secrity.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- styleを改竄して別サイトに飛ばされる可能性がある -->
+  <a
+    :href="sanitizedUrl"
+    :style="userProvidedStyles"
+  >
+    click
+  </a>
+
+  <!-- このようにすると特定のプロパティしか変更できないようにすることができる -->
+  <a
+    :href="sanitizedUrl"
+    :style="{
+      color: userProvidedColor,
+      background: userProvidedBackground
+    }"
+  >
+    click
+  </a>
+</template>
+
+<script setup lang="ts">
+// NG
+Vue.createApp({
+  temolate: `<div>${ userProvidedString }</div>`
+}).mount('#app')
+
+// Example(escape)
+<h1>{{ userProvidedString }}</h1>
+</script>
