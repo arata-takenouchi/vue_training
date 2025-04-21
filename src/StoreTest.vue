f7d7efcfa594a55fdd82a7373f7b350b34ef4807
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-04-21 14:35:17 +0900

    store 1

---
+++ src/StoreTest.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>StoreTest</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { CounterStore } from '@/stores/counter'
+import { storeToRefs } from 'pinia'
+import { computed } from 'vue'
+
+const store = CounterStore()
+// NG storeの中身はreactiveオブジェクトなので、これをやるとreactiveが失われてただのプロパティになる
+const { name, daubleCount } = store
+// こうすればreactiveを保持してプロパティを抽出できる（メソッド使わずプロパティだけ欲しい場合は有用）
+const { name, daubleCount } = storeToRefs(store)
+</script>
