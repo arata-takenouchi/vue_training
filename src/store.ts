3d12ba5291633e81a6c6948801c44ae2fedd9e72
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-04-22 23:33:02 +0900

    setup store

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,13 @@
-import { reactive } from 'vue'
+import { defineStore } from "pinia";
+import { computed, ref } from "vue";
 
-export const store = reactive({
-  count: 0
+export const useCounterStore = defineStore('counter', () => {
+  const count = ref(0)
+  const name = ref('Eduardo')
+  const doubleCount = computed(() => count.value * 2)
+  function increment() {
+    count.value++
+  }
+
+  return { count, name, doubleCount, increment }
 })
