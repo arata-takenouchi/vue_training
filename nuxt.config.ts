778805d59d7839ff2a89ff48f2a8a9b91b245385
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-04-26 14:17:22 +0900

    storeで読み込むフォルダを追加する場合

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@ import { defineNuxtConfig } from "nuxt/config";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@pinia/nuxt"]
+  modules: ["@pinia/nuxt"],
+  pinia: {
+    storeDirs: ['./stores/**', './custom-folder/stores/**']
+  },
 })
